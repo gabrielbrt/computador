afd0a92f601c7850a6bce0f83e67683055e5c0d2
gabrielbrt <curso.livre@belasartes.edu.br>
2017-03-07 20:23:44 -0300

    adicionada vista
    adicionada vista com botoes e contador

--- Computador.elm
+++ Computador.elm
@@ -1,3 +1,17 @@
 module main exposing (..)
 
-modelo = 0
+import Html exposing (div, button, text)
+
+--modelo
+
+modelo =
+     0
+
+
+-- Vista
+
+main = div[]
+  [ button [] [text "-"]
+  , text (toString modelo)
+  , button [] [text "+"]
+  ]
